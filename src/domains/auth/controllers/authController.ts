607a28ed64fcb806a0c452614d2cc1821010b721
Fran <francisco.perez@bolttech.io>
2024-07-24 17:35:06 +0200

    Single method to authservice

--- src/domains/auth/controllers/authController.ts
+++ src/domains/auth/controllers/authController.ts
@@ -1,42 +1,37 @@
-import { Request, Response, NextFunction } from 'express';
-import { createTokenService, registerService } from '../services/authService';
+import { Request, Response } from 'express';
+import { authService } from '../services/authService';
+import { authEndpoints } from "../modules/authEndpoints";
+
+type AuthAction = 'login' | 'register';
 
 export const createToken = async (req: Request, res: Response) => {
     try {
-        console.log('Request body: ', req.body);
-        const response = await createTokenService(req.body);
-        res.json(response);
-    } catch (error: unknown) {
-        if (error instanceof Object && 'status' in error && 'message' in error) {
-            console.log('Error in controller:', (error as any).message);
-            res.status((error as any).status).send((error as any).message);
-        } else {
-            console.log('Error in controller:', error);
-            res.status(500).send({ error: 'An unknown error occurred' });
-        }
+        res.json(await handleRequest(req, res, 'login'));
+    } catch (error: any) {
+        handleErrors(error, res);
     }
 }
 
 export const register = async (req: Request, res: Response) => {
     try {
-        console.log('Request body: ', req.body);
-        const response = await registerService(req.body);
-        res.json(response);
-    } catch (error: unknown) {
-        if (error instanceof Object && 'status' in error && 'message' in error) {
-            console.log('Error in controller:', (error as any).message);
-            res.status((error as any).status).send((error as any).message);
-        } else {
-            console.log('Error in controller:', error);
-            res.status(500).send({ error: 'An unknown error occurred' });
-        }
+        res.json(await handleRequest(req, res, 'register'));
+    } catch (error: any) {
+        handleErrors(error, res);
     }
 }
 
-const handleErrors = (error: unknown, res: Response) => {
+const handleRequest = async (req: Request, res: Response, action: AuthAction) => {
+    console.log('Request body: ', req.body);
+    const { method, url } = authEndpoints[action];
+    return await authService(method, url, req.body);
+}
+
+const handleErrors = (error: any, res: Response) => {
     if (error instanceof Object && 'status' in error && 'message' in error) {
-        res.status((error as any).status).send({ error: (error as any).message });
+        console.log('Error in controller:', (error as any).message);
+        res.status((error as any).status).json({ error: (error as any).message });
     } else {
-        res.status(500).send({ error: 'An unknown error occurred' });
+        console.log('Error in controller:', error);
+        res.status(500).json({ error: 'An unknown error occurred' });
     }
 }
